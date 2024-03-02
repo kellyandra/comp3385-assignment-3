2ea0b799345f059dd7d0570c3df73e7648000e6e
kellyandra <ms.kchristian86@gmail.com>
2024-03-02 17:25:47 -0400

    Exercises 1-3 completed.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Database\Seeders;
-
+use  Illuminate\Database\QueryException ;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,9 +14,9 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         \App\Models\User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'admin@comp3385.com',
+     ]);
     }
 }
